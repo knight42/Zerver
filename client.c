16ebbffd97f1f463b6ce4c119f2960e47f190b2f
Knight <Knight42@mail.ustc.edu.cn>
2015-07-20 00:44:59 +0800

    disp server address

--- client.c
+++ client.c
@@ -3,24 +3,26 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 
 int open_clientfd(const char *hostname, uint16_t port)
 {
     int clifd;
     struct hostent *hp;
-    struct sockaddr_in cliaddr;
+    struct sockaddr_in servaddr;
     if((clifd=socket(AF_INET, SOCK_STREAM, 0))<0)
         return -1;
+    // netdb.h
     if((hp=gethostbyname(hostname))==NULL)
         return -2;
-    bzero((char *)&cliaddr, sizeof(cliaddr));
-    cliaddr.sin_family = AF_INET;
+    bzero((char *)&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
     bcopy((char *)hp->h_addr_list[0], 
-          (char *)&cliaddr.sin_addr.s_addr, hp->h_length);
+          (char *)&servaddr.sin_addr.s_addr, hp->h_length);
     
-    cliaddr.sin_port = htons(port);
-    if(connect(clifd, (struct sockaddr*)&cliaddr, sizeof(cliaddr))<0)
+    servaddr.sin_port = htons(port);
+    if(connect(clifd, (struct sockaddr*)&servaddr, sizeof(servaddr))<0)
         return -1;
     return clifd;
 }
@@ -40,7 +42,6 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "Fail!\n");
         exit(1);
     }
-
     ssize_t res;
     while(1){
         printf("enter some text: ");
